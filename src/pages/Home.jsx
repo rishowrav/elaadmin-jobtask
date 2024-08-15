3aeb186fe31226b7a384bfd9af800575ca380c93
rishowrav <rishowrav@gmail.com>
2024-08-15 09:49:56 +0600

    Card Design completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import { FaChevronDown } from "react-icons/fa";
 import { IoSearch } from "react-icons/io5";
+import Card from "../components/Card";
 
 const Home = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <main className="mx-auto container">
-      <section className="flex justify-between gap-10 mt-10">
+    <main className="mx-auto container space-y-10 mb-20">
+      <section className="flex lg:flex-row flex-col justify-between gap-10 mt-10">
         <details className="dropdown">
-          <summary className="btn btn-lg w-72  btn-accent text-xl flex items-center justify-center ">
+          <summary className="btn btn-lg w-full  btn-accent text-xl flex items-center justify-center ">
             Categorize <FaChevronDown />
           </summary>
           <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
@@ -22,9 +23,9 @@ const Home = () => {
           </ul>
         </details>
 
-        <label className="input input-bordered input-lg flex items-center gap-4 flex-1">
+        <label className="input input-bordered input-lg  flex items-center gap-4 flex-1">
           <IoSearch className="text-2xl text-gray-500" />
-          <input type="text" className="grow" placeholder="Search..." />
+          <input type="text" className="grow h-16" placeholder="Search..." />
         </label>
 
         <button
@@ -33,6 +34,13 @@ const Home = () => {
         >
           Price: {toggle ? "Low to High" : "High to Low"}
         </button>
+      </section>
+
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </section>
     </main>
   );
