8c648d956ba73e5a95dfcdfde1d05799f1844725
rishowrav <rishowrav@gmail.com>
2024-08-15 09:18:19 +0600

    Added: Category dorpdown button , search field and sort of pricing button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaChevronDown } from "react-icons/fa";
+import { IoSearch } from "react-icons/io5";
 
 const Home = () => {
-  return <div></div>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <main className="mx-auto container">
+      <section className="flex justify-between gap-10 mt-10">
+        <details className="dropdown">
+          <summary className="btn btn-lg w-72  btn-accent text-xl flex items-center justify-center ">
+            Categorize <FaChevronDown />
+          </summary>
+          <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </details>
+
+        <label className="input input-bordered input-lg flex items-center gap-4 flex-1">
+          <IoSearch className="text-2xl text-gray-500" />
+          <input type="text" className="grow" placeholder="Search..." />
+        </label>
+
+        <button
+          onClick={() => setToggle(!toggle)}
+          className="btn btn-info btn-lg"
+        >
+          Price: {toggle ? "Low to High" : "High to Low"}
+        </button>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
